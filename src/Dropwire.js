59c7dce22e513c88e5ec5fbf7295658166a7fdb3
Nick Carducci <nmcarducci@gmail.com>
2023-01-11 09:33:00 -0500

    Update Dropwire.js

--- src/Dropwire.js
+++ src/Dropwire.js
@@ -1,52 +1,24 @@
 import React from "react";
+//import ReactDOM from "react-dom";
+//import ReactHtmlParser from "react-html-parser";
+//import reactElementToJSXString from "react-element-to-jsx-string";
+//import ReactDOMServer from "react-dom/server";
+//import ExecutionEnvironment from "exenv";
+
+/*class Forward extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return <div ref={this.props.fwdtwe} />;
+  }
+}*/
 
 class Cable extends React.Component {
   constructor(props) {
     super(props);
-    const { style } = props;
-    var S = {};
-    const isPixels = (s, what) => {
-      if (what) return s.substring(s.length - 2, s.length);
-      return s.substring(s.length - 2, s.length) === "px";
-    };
-    const apply = (s) => (S[s] = s.substring(0, s.length - 2));
-    const isPerc = (s, what) => {
-      if (what) return s.substring(s.length - 1, s.length);
-      return s && s.substring(s.length - 1, s.length) === "%";
-    };
-    const applyPerc = (s) => (S[s] = s.substring(0, s.length - 1));
-    if (style) {
-      if (style.height && isPixels(style.height)) {
-        apply(style.height);
-      } else S.height = style.height;
-      if (style.width && isPerc(style.width)) {
-        applyPerc(style.width);
-      } else S.width = style.width;
-
-      if (style.width && isPixels(style.width)) {
-        apply(style.width);
-      } else S.width = style.width;
-      if (style.height && isPerc(style.height)) {
-        applyPerc(style.height);
-      } else S.height = style.height;
-      //auto,min-content
-      //style && style.width && console.log(isPixels(style.width, true)); //S["width"]); //isPixels(style.width, true));
-    }
-
-    //console.log(initwidth);
-    var initheight =
-        !S && !this.props.img //|| (!isNaN(S.width) /* && !isPerc(S.width)*/ && isNaN(S.height))
-          ? "auto"
-          : S.height,
-      initwidth =
-        !S || !isNaN(S.height) ? "auto" : !isNaN(S.width) ? 200 : S.width;
-    //console.log(initheight);
     this.state = {
-      mount: null,
-      optionalheight: initheight,
-      optionalwidth: initwidth,
-      initheight,
-      initwidth,
       limit: [],
       cache: null,
       mountsCount: 0,
@@ -54,77 +26,138 @@ class Cable extends React.Component {
       go: true
     };
     this.page = React.createRef();
+    this.fwdtwe = React.createRef();
   }
+  /*componentDidMount() {
+    if (ExecutionEnvironment.canUseDOM) {
+      this.setState({ go: true }, () => this.checkIfBetween());
+    }
+  }*/
   componentDidUpdate = (prevProps) => {
-    if (this.state.mount !== this.state.lastmount)
-      this.setState({ lastmount: this.state.mount }, () => {
-        if (this.state.mount)
-          this.setState(
-            { mount: this.props.fwd && this.props.fwd.current },
-            () => {}
-          );
-      });
-    if (
-      (this.state.go && this.props.scrolling !== prevProps.scrolling) ||
-      this.state.loaded !== this.state.lastLoaded
-    )
+    if (this.state.go && this.props.scrolling !== prevProps.scrolling) {
+      this.checkIfBetween();
+    }
+    if (this.state.loaded !== this.state.lastLoaded) {
       this.setState(
         {
           lastLoaded: this.state.loaded
         },
         this.checkIfBetween
       );
+    }
   };
   componentWillUnmount = () => {
-    clearTimeout(this.dyntime3);
     clearTimeout(this.setset);
   };
   checkIfBetween = () => {
-    console.log("scroll");
-    const { cache } = this.state;
-    const { scrollTop, girth, timeout } = this.props;
-    var girt = girth && !isNaN(girth) ? girth + 500 : window.innerHeight;
+    const { /*frameheight,*/ cache } = this.state;
+    const { /*scrollTopAndHeight,*/ scrollTop, girth, timeout } = this.props;
+    var girt =
+      girth && !isNaN(girth)
+        ? girth + 500
+        : window.innerHeight; /*frameheight
+        ? frameheight
+        : Style &&
+          Style.height &&
+          !isNaN(Style.height)
+        ? Style.height + 500
+        : 500;*/
     var timeou = timeout ? timeout : 1500;
     clearTimeout(this.setset);
     this.setset = setTimeout(() => {
       var page = this.page.current;
-      var between = Math.abs(page.offsetTop - scrollTop) < girt;
+      var between =
+        //Math.abs(scrollTop + page.offsetTop - window.scrollY) <
+        //girt + window.innerHeight;
+        Math.abs(page.offsetTop - scrollTop) < girt;
+      /*Number(`-${girt}`) &&
+        scrollTopAndHeight - page.offsetTop > Number(`-${girt}`);*/
+      /* Math.abs(
+            scrollTop +
+              page.offsetTop -
+              (window.scrollY +
+                window.innerHeight /
+                  2) /*+ window.innerHeight / 2 - page.offsetTop*
+          )
+          Math.abs(
+          scrollTop +
+            page.offsetTop -
+            (window.scrollY +
+              window.innerHeight /
+                2) /*+ window.innerHeight / 2 - page.offsetTop*
+        ) < girt; //Number(`-${girt}`);*/
+      //console.log(page.offsetTop);
       if (!this.state.mount) {
-        this.setState(
-          {
-            mount: between
-          },
-          () => {}
-        );
+        /*console.log(
+          Math.abs(scrollTop + page.offsetTop - window.scrollY),
+          scrollTop,
+          page.offsetTop,
+          window.scrollY ,
+          girt
+        );*/
+        //console.log(between, page.offsetTop, scrollTop);
+        /*between && */ this.setState({ mount: between }, () => {});
       } else {
         var continuee = this.props.fwd && this.props.fwd.current;
-        if (!continuee && !cache)
-          return this.setState({ mount: false }, () =>
-            console.log(continuee.offsetWidth)
-          );
-        if (!cache) {
-          // && this.props.img) {
+        //between && console.log(between, continuee.outerHTML);
+        if (!continuee && !cache) return;
+        /*const cacheStyle = JSON.parse(
+          (cache ? cache : continuee.outerHTML)
+            .split(`style="`)[1]
+            .split(`"`)[0]
+            .replaceAll(";", `",`)
+            .replaceAll(": ", `: "`)
+        );*/
+        //console.log(cache, continuee.offsetHeight, continuee.offsetWidth);
+        if (!cache && (this.state.loaded || this.props.img)) {
+          //if (continuee.offsetHeight !== 0)
           this.setState({
             cache: continuee.outerHTML,
+            //cacheStyle,
             frameheight: continuee.offsetHeight,
             framewidth: continuee.offsetWidth
           });
         } else if (!between) {
-          return null; //this.setState({ mount: false });
-          /*if (continuee) {
+          //console.log("!between", continuee.outerHTML);
+          /* if (continuee) {
+                const children = [...continuee.children];
+                console.log(children);
+                if (children.length > 0) {
+                  var gl = null;
+                  const foun = children.find(
+                    (x) => (gl = x.getContext("webgl"))
+                  );
+                  foun.addEventListener(
+                    "webglcontextlost",
+                    (e) => console.log(e),
+                    false
+                  );
+                  gl.getExtension("WEBGL_lose_context").loseContext();
+                }
+              }*/
+          //continuee.remove();
+          //if (scrollTop !== 0) return;
+          //continuee && continuee.remove();
+          if (continuee) {
             while (continuee.children.length > 0) {
               continuee.remove(
                 continuee.children[continuee.children.length - 1]
               );
             }
-          }*/
-        } else {
+          }
+          // console.log(girt);
+          //if (Object.keys(page.children).length !== 0 /*page.innerHTML !== ""*/)
+          //return (page.innerHTML = "");
+          // this.setState({ mount: false });
+        } /*if (page.innerHTML === "") */ else {
           const children = [...page.children];
           if (
+            //frusterated the second, paniced the first" ca
             cache &&
             (children.length === 0 || !children.find((x) => x === cache))
           ) {
             console.log("reloading");
+            //console.log("replenishing, new scroll", cache);
             return (page.innerHTML = this.state.cache);
           }
         }
@@ -132,72 +165,79 @@ class Cable extends React.Component {
     }, timeou);
   };
   render() {
-    const { mount } = this.state;
-    const { src, float, title, img } = this.props;
+    const { mount, stopfail } = this.state;
+    const { src, float, title, img, style: Style } = this.props;
     const onError = (e) => {
       this.setState({ stopfail: true });
+      //this.props.fwd.current.remove();
       this.props.onError(e);
-    };
+    }; //ternaries remove the node (error) and element;
+    //display removes the element, but not the node; use state
     const onLoad = (e) => {
-      console.log("loaded (iframe) [drop]wire");
+      console.log("loaded");
       this.setState({
         loaded: true
       });
     };
-    const style = {
-      border: "0px gray solid",
-      width: this.state.resizing ? this.state.framewidth : null
-      //height: this.state.resizing ? this.state.frameheight : 20
-    };
+
+    var optionalheight = "auto";
+    var optionalwidth = 200;
+    if (Style) {
+      optionalheight =
+        !stopfail && Style.height && !isNaN(Style.height)
+          ? this.props.img
+            ? "100%"
+            : "auto"
+          : Style.height && this.state.loaded
+          ? Style.height
+          : "auto";
+      optionalwidth = !stopfail && Style.width ? Style.width : 200;
+    }
     return (
       <div
         ref={this.page}
         style={{
-          transition: ".3s ease-out",
           textAlign: float,
           position: "relative",
           boxShadow: "inset 0px 0px 50px 15px rgb(200,100,120)",
-          ...this.props.style,
-          ...style,
-          //shapeOutside: "rect()",
+          ...Style,
+          shapeOutside: "rect()",
           float,
-          overflow: "hidden" //this.state.mount ? "hidden" : "auto"
+          overflow: "hidden",
+          height: optionalheight,
+          width: optionalwidth
         }}
       >
-        {src === "" ? (
-          <span style={style}>{title}</span>
-        ) : !img ? (
-          <iframe
-            onLoad={onLoad}
-            onError={onError}
-            title={title}
-            style={{
-              ...style,
-              border: 0,
-              width: this.state.initwidth,
-              /*eidth:                this.state.optionalwidth !== 200
-                  ? this.state.initwidth
-                  : "100%"*/
-              height: this.state.initheight
-            }}
-            ref={this.props.fwd}
-            src={src}
-            iframe={{ ...this.props.iframe }}
-          />
-        ) : mount ? (
+        {src === "" || (!img && !mount) ? (
+          <span style={{ border: "2px gray solid" }}>{title}</span>
+        ) : img ? (
           <img
             onError={onError}
             alt={title}
             style={{
-              ...style,
-              width: this.state.initwidth,
-              height: this.state.initheight
+              position: "relative",
+              border: src === "" ? "2px gray solid" : 0,
+              height: Style && !isNaN(Style.width) ? "auto" : optionalheight,
+              width: Style && !isNaN(Style.height) ? "auto" : optionalwidth
             }}
             ref={this.props.fwd}
             src={src}
           />
         ) : (
-          <span style={{ border: "2px gray solid" }}>{title}</span>
+          <iframe
+            onLoad={onLoad}
+            onError={onError}
+            title={title}
+            style={{
+              position: "relative",
+              border: 0,
+              height: optionalheight,
+              width: "100%"
+            }}
+            ref={this.props.fwd}
+            src={src}
+            iframe={{ ...this.props.iframe }}
+          />
         )}
       </div>
     );
